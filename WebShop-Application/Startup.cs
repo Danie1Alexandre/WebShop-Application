4d4d01c1d3b71906f6af5f3b8953f22fc8428eec
Daniel Alexandre <dani.alex@live.se>
2022-01-12 06:54:18 +0100

    2

--- WebShop-Application/Startup.cs
+++ WebShop-Application/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -15,12 +17,22 @@ namespace WebShop_Application
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+       public   IConfiguration Configuration { get; }
+        public Startup (IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllersWithViews();
 
-            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
-            services.AddScoped<IPieRepository, MockPieRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IPieRepository, PieRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
